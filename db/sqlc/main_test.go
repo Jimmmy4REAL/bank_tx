5129ba886796c44c3b85880b6737369039faddbc
Jimmmy Ye <jimmmy@Jimmys-MacBook-Pro.local>
2023-08-08 12:07:35 +0100

    completed main part of api layer, moving to paseto-token implementation, improving system security

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Jimmmy4REAL/bank_tx/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/bank_tx?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -19,13 +15,17 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(testDB) // store the testQueries for reuse
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/Jimmmy4REAL/bank_tx/api"
+	db "github.com/Jimmmy4REAL/bank_tx/db/sqlc"
+	"github.com/Jimmmy4REAL/bank_tx/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
